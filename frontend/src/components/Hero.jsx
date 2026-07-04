9450b818c9cbdbc1f668585e4cdb57856ce151f8
Apple <apple@Apples-Air.lan>
2026-07-04 12:36:41 +0100

    Reduce hero profile image size

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <img
           src="https://tse4.mm.bing.net/th/id/OIP.46kBfNqdOveb1FS1c_V9VwHaHa?cb=thfvnextfalcon2&w=980&h=980&rs=1&pid=ImgDetMain&o=7&rm=3"
           alt="profile"
-          className="w-40 h-40 rounded-full mx-auto border-4 border-white"
+          className="hero-profile-image"
         />
 
         <h1 className="text-5xl font-bold mt-6">
